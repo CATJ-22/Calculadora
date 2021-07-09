66f15fc808d292d79eec92b34210ecf97ead17c1
Christian Terrones <65476107+CATJ-22@users.noreply.github.com>
2021-07-09 13:53:01 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.io.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        //Definición de Variables
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))
+        float num1, num2 = 0;
+        char operador = ' ';
+
         System.out.println("Hello, World!");
     }
 }
